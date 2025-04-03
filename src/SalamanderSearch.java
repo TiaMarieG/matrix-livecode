8dc6843744d3d9dcb11654211c03fc27c42a94ba
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-03 14:49:40 -0700

    Code along with Auberon

--- src/SalamanderSearch.java
+++ src/SalamanderSearch.java
@@ -43,6 +43,107 @@ public class SalamanderSearch {
      * @return whether the salamander can reach the food
      */
     public static boolean canReach(char[][] enclosure) {
+        
+        // Finding the salamander
+        int[] start = salamanderLocation(enclosure);
+
+        // Boolean array to check if a node has been visited
+        // Automatically starts with everything false
+        boolean[][] visited = new boolean[enclosure.length][enclosure[0].length];
+
+        return canReach(enclosure, start, visited);
+    }
+
+    // Helper method to check if an index in the 2D array has been visited
+    public static boolean canReach(char[][] enclosure, int[]current, boolean[][] visited) {
+
+        // Base cases - If salamander has been here, return false
+        int curR = current[0];
+        int curC = current[1];
+
+        // If already visited (visited == true)
+        if (visited[curR][curC]) return false;
+
+        // If food is found
+        if(enclosure[curR][curC] == 'f') return true;
+
+        // Mark the locations as visited
+        visited[curR][curC] = true;
+
+        // Look at neighbors
+        List<int[]> possibleMoves = possibleMoves(enclosure, current);
+
+        // Do a for loop to look at each surrounding position
+        for (int[] move : possibleMoves) {
+            // If path can be moved to, no need to keep checking - move
+            if (canReach(enclosure, move, visited)) return true;
+        }
+
+        // If salamander can't move
         return false;
+    }
+
+    //Helper method to determine where the salamander can move given its current location
+    public static List<int[]> possibleMoves(char[][] enclosure, int[] currentLocation) {
+
+        // 0/1 are the indexes of a standard array
+        int curR = currentLocation[0];
+        int curC = currentLocation[1];
+
+        // Create a new list of integer arrays
+        // Stores up to 4 pair values of where salamander can move
+        List<int[]> moves = new ArrayList<>();
+
+        // Checking UP
+        int newR = curR - 1;
+        int newC = curC;
+
+        // Checking to see if it's okay to move up
+        if (newR >= 0 && enclosure[newR][newC] != 'W') {
+            moves.add(new int[]{newR, newC});
+        }
+
+        // Checking DOWN
+        newR = curR + 1;
+        newC = curC;
+
+        if (newR < enclosure.length && enclosure[newR][newC] != 'W') {
+            moves.add((new int[]{newR, newC}));
+        }
+
+        // Checking LEFT
+        newR = curR;
+        newC = curR - 1;
+
+        if (newC >= 0 && enclosure[newR][newC] != 'W') {
+            moves.add(new int[]{newR, newC});
+        }
+
+        // Checking Right
+        newR = curR;
+        newC = curR + 1;
+
+        if (newC < enclosure[0].length && enclosure[newR][newC] != 'W') {
+            moves.add(new int[]{newR, newC});
+        }
+
+        return moves;
+    }
+
+    // Helper method to find where the salamander is as within the 2D array
+    public static int[] salamanderLocation(char[][] enclosure) {
+
+        // Use a nested for loop to check through rows/columns to find the salamander
+        for (int r = 0; r < enclosure.length; r++) {
+            // For rectangular arrays you can use enclosure[0]
+            // For a jagged array, use enclosure[r].length
+            for (int c = 0; c < enclosure[0].length; c++) {
+                if (enclosure[r][c] == 's') {
+                    return new int[]{r, c};
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("No salamander present");
     }
 }
